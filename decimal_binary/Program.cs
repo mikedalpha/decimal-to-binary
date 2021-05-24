d7f2a71d8cf0ba1911b5f7e767d435634eff8e91
Michael Athanasoglou <mikedalpha@gmail.com>
2021-05-24 16:18:42 +0300

    first commit

---
+++ decimal_binary/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class decimal_binary
+{
+    public static void Main(string[] args)
+    {
+        //Main Menu
+        Console.WriteLine("Would you like to convert (Press 1 or 2):\n1.Decimal to Binary?\n-OR-\n2.Binary to Decimal?");
+        string userInput = Console.ReadLine();
+        switch (userInput)
+        {
+            case "1":
+                Console.WriteLine("Enter a decimal:");
+                Console.WriteLine(ToBinary(int.Parse(Console.ReadLine())));
+                break;
+            case "2":
+                Console.WriteLine("Enter a binary:");
+                Console.WriteLine("Decimal is: {0}", ToDecimal(int.Parse(Console.ReadLine())));
+                break;
+            default:
+                break;
+        }
+    }
+
+    static string ToBinary(int number)
+    {
+        //Decimal To Binary
+        string binary = "";
+        int remainder;
+        if (number == 0)
+            return "Binary is: 0";
+        else while (number > 0)
+            {
+                remainder = number % 2;
+                number /= 2;
+                binary = remainder.ToString() + binary;
+            }
+        return ($"Binary is: {binary}");
+    }
+
+    static int ToDecimal(int number)
+    {
+        //Binary To Decimal
+        //Initialize the power of 2 at 1, which is 2^0
+        int power = 1;
+        int result = 0;
+        int remainder;
+        if (number == 0)
+            return 0;
+        else while (number > 0)
+            {
+                remainder = number % 10;
+                number /= 10;
+                result += remainder * power;
+                power *= 2; //by multiplying by 2 each time, we essentially increase the power of 2 by one and achieve the following: 2^0 -> 2^1 -> 2^2 etc
+            }
+        return result;
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Decimal to Binary - Binary to Decimal
+
+This simple console app written in C# is a **binary to decimal - decimal to binary converter.**
+
+The user is prompted as to what function is desired. Then the input is converted to binary if decimal, and vice versa.
+
+This assignment was part of the [PeopleCert Education](https://peoplecert.org) coding bootcamp's introductory exercises in C#.
